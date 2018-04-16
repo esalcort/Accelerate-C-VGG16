9ca01cc37777593edc86bde87ec3ce009bd639cd
esalcort <32207831+esalcort@users.noreply.github.com>
2018-04-16 17:06:08 -0500

    Separated source code for Vivado HLS (compiling)
    To compile use:
    gcc -O3 -fopenmp -lm -o vgg16 vgg16.c convolution_3_x_3.c -I.

---
+++ vivado_hls/convolution_3_x_3.c
@@ -0,0 +1,29 @@
+#include "vgg16.h"
+
+void convolution_3_x_3(float **matrix, float **kernel, float **out, int size) {
+	int i, j;
+	float sum;
+	float zeropad[SIZE + 2][SIZE + 2] = { 0.0 };
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			zeropad[i + 1][j + 1] = matrix[i][j];
+		}
+	}
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			sum = zeropad[i][j] * kernel[0][0] +
+				zeropad[i + 1][j] * kernel[1][0] +
+				zeropad[i + 2][j] * kernel[2][0] +
+				zeropad[i][j + 1] * kernel[0][1] +
+				zeropad[i + 1][j + 1] * kernel[1][1] +
+				zeropad[i + 2][j + 1] * kernel[2][1] +
+				zeropad[i][j + 2] * kernel[0][2] +
+				zeropad[i + 1][j + 2] * kernel[1][2] +
+				zeropad[i + 2][j + 2] * kernel[2][2];
+			out[i][j] += sum;
+		}
+	}
+	
+}
---
+++ vivado_hls/vgg16.h
@@ -0,0 +1,12 @@
+#ifndef VGG16_H
+#define VGG16_H
+
+#include <math.h>
+
+#define SIZE 224
+#define CONV_SIZE 3
+
+// External function prototypes
+void convolution_3_x_3(float **, float **, float **, int);
+
+#endif
