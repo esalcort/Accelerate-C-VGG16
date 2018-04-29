4343fb0672150814b93ec0dad341e5320b04bf7f
esalcort <32207831+esalcort@users.noreply.github.com>
2018-04-29 17:42:55 -0500

    Revert "Added fpga communications"
    This reverts commit 2303374bc2fcd8b20ca58ee234c3d2d7b60fb4e7.

--- use_accelerator/vgg16.h
+++ use_accelerator/vgg16.h
@@ -9,10 +9,11 @@
 #define SIZE 224
 #define CONV_SIZE 3
 
+// External function prototypes
+// void convolution_3_x_3(float matrix[SIZE+2][SIZE+2], float kernel[CONV_SIZE][CONV_SIZE], float out[SIZE+2][SIZE+2], int size);
+// void reset_fpga(int size);
+// void join_fpga(float out[SIZE+2][SIZE+2], int size);
 void convolution_2d(int shape_depth, float input[][SIZE + 2][SIZE + 2], float weights_kernel[][CONV_SIZE][CONV_SIZE],
 					float output[SIZE + 2][SIZE + 2], int size);
 
-void set_arguments(float matrix[SIZE+2][SIZE+2], float kernel[CONV_SIZE][CONV_SIZE], float out[SIZE+2][SIZE+2], int size);
-void fpga_start();
-void fpga_poll();
 #endif
